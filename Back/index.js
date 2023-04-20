f27ff5f0b50fefe3d190c2ba104f8f9eaff60cc3
Júlia Gomes <juliamgomes1@gmail.com>
2023-04-19 22:13:55 -0300

    integração com mysql

--- Back/index.js
+++ Back/index.js
@@ -3,6 +3,23 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'localhost', // O host do banco. Ex: localhost
+    user: 'root', // Um usuário do banco. Ex: user 
+    password: '', // A senha do usuário. Ex: user123
+    database: 'db_extcomp' // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
+});
+
+con.connect((err) => {
+    if (err) {
+        console.log('Erro connecting to database...', err)
+        return
+    }
+    console.log('Connection established!')
+})
+
 
 router.get('/', function(req,res){
     res.sendFile(path.join(__dirname+'/../Front/index.html'))
@@ -11,3 +28,17 @@ router.get('/', function(req,res){
 
 app.use('/',router);
 app.listen(process.env.port || 3000);
+
+con.query('SELECT * FROM curso', (err, rows) => {
+    if (err) throw err
+
+    console.log('Authors: ', rows, '\n\n')
+})
+
+con.end((err) => {
+    if(err) {
+        console.log('Erro to finish connection...', err)
+        return 
+    }
+    console.log('The connection was finish...')
+})
