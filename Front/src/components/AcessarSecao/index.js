365ba3878829ed0842c6f3113888acd219ce1be5
GabrielmAlves <gabrielmalves10@gmail.com>
2023-05-20 13:42:43 -0300

    Lógica da descrição dos cursos feita. Preciso fazer ajustes ainda.

--- Front/src/components/AcessarSecao/index.js
+++ Front/src/components/AcessarSecao/index.js
@@ -10,9 +10,27 @@ import config from "../../../config/config.json";
 
 export default function AcessoSecao(props) {
 
+  const [descricao, setDescricao] = useState("");
+
   const handlePress = () => {
     Linking.openURL(props.url);
   };
+
+
+  async function descricao() {
+    let req = await fetch(config.urlRootNode + "curso", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        desc: descricao,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setDescricao(data.cursos)); //onde setaria a descrição do curso clicado
+  }
   
   return (
     <View style={styles.Container}>
@@ -22,7 +40,7 @@ export default function AcessoSecao(props) {
       <Text style={styles.titulo}>{props.titulo}</Text>
       <View style={styles.containerBtns}>
         <TouchableOpacity> 
-          <Icons name="info-circle" size={35} color="orange" />
+          <Icons name="info-circle" size={35} color="orange" onPress={descricao} />
         </TouchableOpacity>
         <TouchableOpacity onPress={handlePress} >
           <Icons name="arrow-right" size={35} color="orange"  />
