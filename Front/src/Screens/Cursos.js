c1a9c2888be257d8ed9a481caeb9d2230f81985c
Júlia Gomes <juliamgomes1@gmail.com>
2023-06-20 17:35:36 -0300

    ajustado

--- Front/src/Screens/Cursos.js
+++ Front/src/Screens/Cursos.js
@@ -1,21 +1,29 @@
-import { FlatList, StyleSheet, Text, View, TextInput, TouchableOpacity,ScrollView } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import AvisoSemConteudo from "../components/SemConteudo";
 import HeaderNavigacao from "../components/HeaderNavigacao";
 import AcessoSecao from "../components/AcessarSecao";
 import Icons from "react-native-vector-icons/FontAwesome";
 import { useEffect, useState } from "react";
 import config from "../../config/config.json";
-import stylesFilter from "../components/InputDeFiltro/style"
-
-
+import stylesFilter from "../components/InputDeFiltro/style";
 
 export default function Cursos() {
-  
-  const [allCourses, setAllCourses] = useState(fetch(config.urlRootNode + "cursos")
-  .then((res) => res.json())
-    .then((json) =>{
-    setAllCourses(json.cursos)}));
+  const [allCourses, setAllCourses] = useState([]);
   const [filtro, setFiltro] = useState("");
+
+  useEffect(() => {
+    fetch(config.urlRootNode + "cursos")
+      .then((res) => res.json())
+      .then((json) => setAllCourses(json.cursos));
+  }, []);
 
   async function busca() {
     let req = await fetch(config.urlRootNode + "buscaCurso", {
@@ -36,32 +44,33 @@ export default function Cursos() {
     <View style={styles.container}>
       <HeaderNavigacao back="Home" />
       <View style={stylesFilter.FilterArea}>
-          <TextInput
-            style={stylesFilter.formFilter}
-            placeholder="Faça uma pesquisa"
-            onChangeText={(text) => setFiltro(text)}
-          />
-          <TouchableOpacity onPress={busca}>
-          <Icons name="search" size={25} color="orange"/>
-          </TouchableOpacity>
-        </View>
-        {
-   
-   allCourses.length > 0 ? (
-    <ScrollView style={styles.scroll}>
-     <View style={styles.contentArea}> 
-   {   allCourses.map(item => (
-         <AcessoSecao titulo={item.titulo} url={item.url} descricao={item.descricao} dataExp={item.data_exp} back="Cursos" img={item.logo}/>
-     ))}
- </View>
- </ScrollView>
- )   :   <AvisoSemConteudo text="cursos" />
-
- 
-}
-
-     
-      
+        <TextInput
+          style={stylesFilter.formFilter}
+          placeholder="Faça uma pesquisa"
+          onChangeText={(text) => setFiltro(text)}
+        />
+        <TouchableOpacity onPress={busca}>
+          <Icons name="search" size={25} color="orange" />
+        </TouchableOpacity>
+      </View>
+      {allCourses.length > 0 ? (
+        <ScrollView style={styles.scroll}>
+          <View style={styles.contentArea}>
+            {allCourses.map((item) => (
+              <AcessoSecao
+                titulo={item.titulo}
+                url={item.url}
+                descricao={item.descricao}
+                dataExp={item.data_exp}
+                back="Cursos"
+                img={item.logo}
+              />
+            ))}
+          </View>
+        </ScrollView>
+      ) : (
+        <AvisoSemConteudo text="cursos" />
+      )}
     </View>
   );
 }
@@ -73,17 +82,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
     marginTop: 30,
-  
   },
   contentArea: {
     flexDirection: "row",
     justifyContent: "space-around",
     width: "95%",
     flexWrap: "wrap",
-  
   },
-  scroll:{
-    width:"100%",
-    marginLeft:15
-  }
+  scroll: {
+    width: "100%",
+    marginLeft: 15,
+  },
 });
